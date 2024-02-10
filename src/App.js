207778ffbd2dcfb247e240a35087e67fbc367fac
reginaldolimas <reginaldolimas@gmail.com>
2024-02-10 19:25:59 -0300

    melhoria no routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import './App.css';
-/* import BreadCrumb from './components/BreadCrumb/BreadCrumb';
-import LayoutDefault from './components/Layout/LayoutDefault'; */
 
 function App() {
   return (
     <div className="appBg">
       <div className='login-container'>
-      {/*   <BreadCrumb />
-        <LayoutDefault /> */}
       </div>
     </div>
   );
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
 import ErrorPage from "./error-page";
-import Contact from "./routes/contact";
-import Root, {loader as rootLoader} from "./routes/root";
-import { action as rootAction } from "./routes/root";
 import { Login } from "./components/Login/Login";
 import LayoutDefault from "./components/Layout/LayoutDefault";
 
@@ -11,21 +8,11 @@ export const router = createBrowserRouter([
         path: "/",
         element: <Login />,
         errorElement: <ErrorPage />,
-        loader: rootLoader,
-        action: rootAction,
-        children: [
-            {
-                path: "/contacts/:contactId",
-                element: <Contact />,
-            },
-        ]
     },
     {
         path: "/home",
         element: <LayoutDefault />,
         errorElement: <ErrorPage />,
-        loader: rootLoader,
-        action: rootAction,
     },
 
 ]);
